b74159a9f6460d9d369ae6597a8baba7b2a4f741
Charles Onyango <51433913+sjarelkral@users.noreply.github.com>
2020-06-11 22:25:15 +0900

    cleanup

--- Microprocessor/Microprocessor.v
+++ Microprocessor/Microprocessor.v
@@ -44,16 +44,18 @@ module Microprocessor(
     end
     assign clock = sec;
 
-    //7-segment display
-    assign op = ir[7:6];
-
-
     //Storage elements
     reg [7:0]registers[3:0];
     reg [7:0]pc;
     reg [7:0]ir;
     reg [7:0]memory[31:0];
-    reg
+    reg [4:0]rw_num;
+    reg console_buffer;
+
+    //7-segment display
+    Console data1(rwd_1, console_buffer[7:4]);
+    Console data0(rwd_0, console_buffer[3:0]);
+    Console reg_num_(reg_num, rw_num);
 
     //buses and wires
     wire [7:0]literal;
@@ -64,12 +66,8 @@ module Microprocessor(
     assign mem_write = (op == 2'b10);
     assign mem_read = (op == 2'b01);
     assign reg_write = ~op[1];
-
-
-
-
-
-
+    assign op = ir[7:6];
+    assign console_buffer = registers[rw_num];
 
     always @ (posedge reset or posedge clock) begin
 
@@ -117,95 +115,22 @@ module Microprocessor(
 
     else begin
 
-    // PC <== pc + 1 + (offset /zero)
-    pc <= pc +1 + ((op == 2'd11)?immediate:8'd0);
+        // PC <== pc + 1 + (offset<jump> /zero)
+        pc <= pc +1 + ((op == 2'd11)?immediate:8'd0);
+
+        if (op == 2'd00) begin
+          registers[ir[1:0]] <= registers[ir[3:2]]+registers[ir[5:4]];
+        end
+
+        else if (op == 2'd01) begin
+          registers[ir[3:2]] <= memory[registers[ir[5:4]]+immediate];
+        end
+
+        else if (op == 2'd10) begin
+          memory[registers[ir[5:4]]+immediate] <= registers[ir[3:2]];
+
+        end
 
     end
-
-    end
-
-    reg [7:0]console_buffer;
-
-    Console disp1(high_out, console_buffer[7:4]);
-    Console disp2(low_out, console_buffer[3:0]);
-
-	 //Buses
-	 wire RegDst;
-   wire RegWrite;
-   wire ALUSrc;
-   wire Branch;
-   wire MemRead;
-   wire MemWrite;
-   wire MemtoReg;
-   wire ALUOp;
-   wire [1:0]ReadRegister1;
-   wire [1:0]ReadRegister2;
-   wire [1:0]WriteRegister;
-	 wire [7:0]MemAddress;
-   wire [7:0]ALUResult;
-   wire [7:0]ALUin1;
-   wire [7:0]ALUin2;
-	 wire [7:0]ReadData1; //reg
-	 wire [7:0]ReadData2; //reg
-	 wire [7:0]ReadData; //mem
-	 wire [7:0]RegWriteData;
-	 wire [7:0]WriteData;
-
-   //PC
-   assign PCAddess = PC; // Instruction  = Instruction Memory[PC]
-
-   //Instruction OP code is used to generate control signals
-   assign RegDst =  ~IR[6];
-	 assign RegWrite = ~IR[7];
-	 assign ALUSrc = IR[7] ^ IR[6];
-	 assign Branch  = IR[7]& IR[6];
-	 assign MemRead = (IR[7:6] == 2'b01 );
-	 assign MemWrite = (IR[7:6] == 2'b10);
-	 assign MemtoReg = IR[6];
-	 assign ALUOp = ~(IR[7] | IR[6]);
-
-   //Register input/output connections
-   assign ReadRegister1 = IR[5:4];
-   assign ReadRegister2 = IR[3:2];
-   assign WriteRegister = RegDst ? IR[1:0] : IR[3:2];
-   assign RegWriteData = (MemtoReg) ? ReadData : ALUResult;
-
-   assign ReadData1 = registers[ReadRegister1];
-
-    assign ReadData2 = registers[ReadRegister2];
-
-   //connections into the ALU
-   assign ALUin1  = ReadData1;
-   assign ALUin2 = (ALUSrc) ? SignExtImm : ReadData2;
-   assign ALUResult = ALUin1 + ALUin2;
-
-   //Memory input/output values
-   assign MemAddress = ALUResult;
-   assign WriteData = ReadData2;
-   assign ReadData = DataMemory[MemAddress];
-
-   always @ ( posedge clock or posedge reset) begin
-
-       else begin
-
-       IR <= Instruction;
-       PC <= Branch ? PC+1+SignExtImm : PC+1; //Evaluate PC
-
-       if (RegWrite) begin
-         case (WriteRegister)
-           2'd0: GPR[0] <= RegWriteData;
-           2'd1: GPR[1] <= RegWriteData;
-           2'd2: GPR[2] <= RegWriteData;
-           2'd3: GPR[3] <= RegWriteData;
-         endcase
-         console_buffer <= RegWriteData;
-       end
-
-       if (MemWrite) begin
-         DataMemory[MemAddress] <= WriteData;
-       end
-		 end
-
-		 end
 
 endmodule
